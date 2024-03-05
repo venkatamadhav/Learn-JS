bc39eac25307ac66f9a96f2785da745d3f98c3cb
venkatamadhav <venkat.madhav.98@gmail.com>
2024-03-05 20:21:26 +0530

    Added MapClick Event for Mapty

--- 15. Mapty/script.js
+++ 15. Mapty/script.js
@@ -20,9 +20,19 @@ navigator.geolocation.getCurrentPosition(function (position) {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker(coords).addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+    map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng]).addTo(map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+            .setPopupContent('Workout')
+            .openPopup();
+    });
 }, function () {
     alert('Could not get your position');
 });
