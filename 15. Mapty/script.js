40a53baaeb3e0188f4b89e5278a791c2bdb0aa68
venkatamadhav <venkat.madhav.98@gmail.com>
2024-03-07 15:48:20 +0530

    Workouts added in Mapty

--- 15. Mapty/script.js
+++ 15. Mapty/script.js
@@ -11,8 +11,42 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+    date = new Date();
+    id = Date.now();
+    constructor(coords, distance, duration) {
+        this.coords = coords;
+        this.distance = distance;
+        this.duration = duration;
+    }
+}
+class running extends Workout {
+    type = 'running';
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+    calcPace() {
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+class cycling extends Workout {
+    type = 'cycling';
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+    calcSpeed() {
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed;
+    }
+}
+// /////////////////////////////////////////
 class App {
-    #map; #mapEvent;
+    #map; #mapEvent; #workouts = [];
     constructor() {
         this._getPosition();
         form.addEventListener('submit', this._newWorkout.bind(this));
@@ -45,18 +79,56 @@ class App {
     }
     _newWorkout(e) {
         e.preventDefault();
-        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+        const NonEmptyInputs = (...inputs) => inputs.every(inp => inp !== '');
+        const validInputs = (...inputs) => inputs.every(inp => Number.isFinite(inp));
+        const allPositive = (...inputs) => inputs.every(inp => inp > 0);
         const { lat, lng } = this.#mapEvent.latlng;
-        L.marker([lat, lng]).addTo(this.#map)
+        const type = inputType.value;
+        const distance = +inputDistance.value;
+        const duration = +inputDuration.value;
+        let workout;
+        if (type === 'running') {
+            const cadence = +inputCadence.value;
+            console.log(NonEmptyInputs(distance, duration, cadence) && validInputs(distance, duration, cadence) && allPositive(distance, duration, cadence));
+            if (NonEmptyInputs(distance, duration, cadence) &&
+                validInputs(distance, duration, cadence) &&
+                allPositive(distance, duration, cadence)) {
+                workout = new running([lat, lng], distance, duration, cadence);
+            }
+            else {
+                return alert('Please enter proper inputs');
+            }
+        }
+        else {
+            const elevation = +inputElevation.value;
+            if (NonEmptyInputs(distance, duration, elevation) &&
+                validInputs(distance, duration, elevation) &&
+                allPositive(distance, duration)) {
+                workout = new cycling([lat, lng], distance, duration, elevation);
+            }
+            else {
+                return alert('Please enter proper inputs');
+            }
+        }
+        if (workout) {
+            this.#workouts.push(workout);
+            this.renderWorkoutMarker(workout);
+        }
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+    }
+    renderWorkoutMarker(workout) {
+        L.marker(workout.coords).addTo(this.#map)
             .bindPopup(L.popup({
                 maxWidth: 250,
                 minWidth: 100,
                 autoClose: false,
                 closeOnClick: false,
-                className: 'running-popup'
+                className: `${workout.type}-popup`
             }))
             .setPopupContent('Workout')
             .openPopup();
     }
+
 }
 const app = new App();
